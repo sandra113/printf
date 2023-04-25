89cc10f61a344a349f62fd2d51a27d5e6299ec13
Sandra Mabonga <sandramabonga303@gmail.com>
2023-04-25 12:24:51 -0700

    add a negative number

--- print_number.c
+++ print_number.c
@@ -2,29 +2,39 @@
 #include <stdlib.h>
 
 /**
- * print_number - prints a number
- * @list: va_list whose next item is a number
- * Return: Number of bytes printed
+ * number_to_string - converts an int value to string
+ * @num: the integer to be converted
+ * @str: the char pointer that stores the string
+ * @depth: the length of the string
+ * Return: the string
  */
 
 int number_to_string(int num, char **str, int depth)
 {
-	int index;
-	
+	int index, neg_index;
+
+	if (num == 0 && depth > 0)
+	{
+		*str = malloc(sizeof(char) * (depth + 1));
+		(*str)[depth] = '\0';
+		return (0);
+	}
 	if (num == 0)
 	{
-		if (depth == 0)
-		{
-			*str = malloc(sizeof(char) * (2));
-			(*str)[1] = '\0';
-			(*str)[0] = '0';
-			return 1;
-		}
-		*str = malloc(sizeof(char) * (depth +1));
-		(*str)[depth] = '\0';
-		return 0;
+		*str = malloc(sizeof(char) * (2));
+		(*str)[1] = '\0';
+		(*str)[0] = '0';
+		return (1);
 	}
+	if (num < 0)
+	{
+		neg_index = number_to_string(-num, str, depth + 1);
+		memmove(*str + 1, *str, neg_index);
+		(*str)[0] = '-';
+		return (neg_index + 1);
+	}
+
 	index = number_to_string(num / 10, str, depth + 1);
 	(*str)[index] = num % 10 + '0';
-	return index + 1;
+	return (index + 1);
 }
