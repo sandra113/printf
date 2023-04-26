639b3518d76e4a6a9b507bd1ac8ba11ef7a6bce0
= <=>
2023-04-26 20:21:18 +0300

    Comply with betty

--- print_byte.c
+++ print_byte.c
@@ -7,24 +7,25 @@
  */
 int print_byte(va_list list)
 {
-        unsigned int byte = va_arg(list, unsigned int);
-        unsigned int i;
-        unsigned int size = sizeof(int) * 8;
-        char to_be_printed[65];
+	unsigned int byte = va_arg(list, unsigned int);
+	unsigned int i;
+	unsigned int size = sizeof(int) * 8;
+	char to_be_printed[65];
 
-        for (i = 1; i <= size; i++)
-        {
-                if ((byte & (1 << (size - i))))
-                        to_be_printed[i-1] = '1';
-                else
-                        to_be_printed[i-1] = '0';
-        }
-        to_be_printed[i-1] = '\0';
-        i = 0;
-        while (to_be_printed[i+1]){
-                if (to_be_printed[i] == '1')
-                        break;
-                i++;
-        }
-        return (_printf("%s", to_be_printed + i));
-}
+	for (i = 1; i <= size; i++)
+	{
+		if ((byte & (1 << (size - i))))
+			to_be_printed[i - 1] = '1';
+		else
+			to_be_printed[i - 1] = '0';
+	}
+	to_be_printed[i - 1] = '\0';
+	i = 0;
+	while (to_be_printed[i + 1])
+	{
+		if (to_be_printed[i] == '1')
+			break;
+		i++;
+	}
+	return (_printf("%s", to_be_printed + i));
+	}
