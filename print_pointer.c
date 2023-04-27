cbb8ee1c20d3ffef5cebc144404b7f147b45ec93
Sandra Mabonga <sandramabonga303@gmail.com>
2023-04-26 23:32:08 -0700

    corret betty

--- print_pointer.c
+++ print_pointer.c
@@ -3,6 +3,12 @@
 #include <stdarg.h>
 #include "main.h"
 
+/**
+ * print_pointer - prints a pointer value
+ * @list - the va_list whose next argument is a pointer
+ * Return: the value address
+ */
+
 int print_pointer(va_list list)
 {
 	int i;
@@ -10,13 +16,13 @@ int print_pointer(va_list list)
 	char buf[20];
 
 	p = va_arg(list, void *);
-	
+
 	sprintf(buf, "%p", p);
 	for (i = 0; buf[i] != '\0'; i++)
 	{
 		if (buf[i] >= 'a' && buf[i] <= 'z')
 		{
-			buf[i] = buf[i] -'a' + 'A';
+			buf[i] = buf[i] -  'a' + 'A';
 		}
 	}
 	return (write(1, buf, i));
