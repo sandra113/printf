5998823adad10a255c512d6d66a92483b1c8aa58
Sandra Mabonga <sandramabonga303@gmail.com>
2023-04-26 23:26:28 -0700

    print_pointer

---
+++ print_pointer.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+int print_pointer(va_list list)
+{
+	int i;
+	void *p;
+	char buf[20];
+
+	p = va_arg(list, void *);
+	
+	sprintf(buf, "%p", p);
+	for (i = 0; buf[i] != '\0'; i++)
+	{
+		if (buf[i] >= 'a' && buf[i] <= 'z')
+		{
+			buf[i] = buf[i] -'a' + 'A';
+		}
+	}
+	return (write(1, buf, i));
+}
